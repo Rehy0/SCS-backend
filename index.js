d214de0dcc181faf3f03a283f6d62c21f002f97c
Rehan Sheikh <rehansheikh0808@gmail.com>
2025-11-15 18:17:12 +0530

    feat: enhance index.js response formatting; add README.md content; update .gitignore

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send("Hello World!")
+  res.send("<h1>Hello World!</h1>")
+})
+
+app.get('/name',function(req,res){
+res.send("<h1>My name is Rehan</h1>")
+})
+
+app.get('/name/surname',function(req,res){
+res.send("<h1>My surname is Sheikh</h1>")
 })
 
 app.listen(3000)
