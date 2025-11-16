22b10079b4ede81293dc117e4710033514551e62
Rehan Sheikh <rehansheikh0808@gmail.com>
2025-11-16 07:59:49 +0530

    feat: add middleware for logging requests; remove unused surname route

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const app = express()
 
+app.use(function(req,res,next){
+  console.log("middleware chala")
+  next()
+});
+
 app.get('/', function (req, res) {
   res.send("<h1>Hello World!</h1>")
 })
 
 app.get('/name',function(req,res){
 res.send("<h1>My name is Rehan</h1>")
-})
-
-app.get('/name/surname',function(req,res){
-res.send("<h1>My surname is Sheikh</h1>")
 })
 
 app.listen(3000)
