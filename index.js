b6ecfa4df154d364d6fb36c0917a9c1f0b4d2141
Rehan Sheikh <rehansheikh0808@gmail.com>
2025-11-17 14:59:16 +0530

    feat: add cookie-parser dependency and remove unused routes from index.js

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
 const express = require('express')
 const app = express()
-
-app.use(function(req,res,next){
-  console.log("middleware chala")
-  next()
-});
-
-app.get('/', function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
-
-app.get('/name',function(req,res){
-res.send("<h1>My name is Rehan</h1>")
-})
-
-app.listen(3000)
-
